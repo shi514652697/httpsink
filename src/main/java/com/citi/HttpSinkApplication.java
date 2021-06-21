195311b826df8467f3e2b7e5178873a1a1e7ddc1
shi514652697 <ssnegi_4all@yahoo.com>
2021-06-21 14:34:12 -0500

    first commit

---
+++ src/main/java/com/citi/HttpSinkApplication.java
@@ -0,0 +1,13 @@
+package com.citi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpSinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citi/HttpSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
